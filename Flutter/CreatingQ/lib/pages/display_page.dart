26a1fbeca8c42a8e52152ef33f6f12bcc2ffec2a
mumumu3560 <142517194+mumumu3560@users.noreply.github.com>
2023-11-04 03:19:08 +0900

    Add files via upload
    コメントを表示、送信するwidgetを移した。またコメントはshowModalBottomSheetによって表示するようにした。
    TODO: 問題の評価の閲覧、問題の評価を行うことができるようにする。

--- Flutter/CreatingQ/lib/pages/display_page.dart
+++ Flutter/CreatingQ/lib/pages/display_page.dart
@@ -1,9 +1,12 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:share_your_q/image_operations/image_display.dart'; // ImageDisplayScreenが定義されたファイルをインポート
 import 'package:share_your_q/utils/various.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import "package:share_your_q/image_operations/problem_view.dart";
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 //google_admob
 //TODO ビルドリリースの時のみ
@@ -11,6 +14,9 @@ import "package:share_your_q/image_operations/problem_view.dart";
 
 //リストをタップした際に遷移するページ問題が見れる
 //UIはわからない
+
+//テキストを保持する
+String textKeeper = "";
 
 class DisplayPage extends StatefulWidget {
 
@@ -37,6 +43,7 @@ class DisplayPage extends StatefulWidget {
   final String? explanation;
 
   final int? num;
+  
 
 
   const DisplayPage({
@@ -70,6 +77,7 @@ class DisplayPage extends StatefulWidget {
 }
 
 class _DisplayPageState extends State<DisplayPage>{
+
 
   final userId = supabase.auth.currentUser!.id;
   bool isLiked = false;
@@ -173,66 +181,80 @@ class _DisplayPageState extends State<DisplayPage>{
   }
 
   Future<void> _insertOrUpdateDataToSupabaseTable() async {
-  try {
-    // `user_id`と`image_id`の組み合わせで既存のレコードを検索する
-    final existingRecord = await supabase
-        .from('likes')
-        .select<List<Map<String, dynamic>>>()
-        .eq('user_id', userId)
-        .eq('image_id', widget.image_id);
-
-    // レコードが存在する場合はアップデート、存在しない場合は挿入する
-    if (existingRecord.isNotEmpty) {
-      // レコードが存在する場合はアップデート
-      print("ここが問題手の");
-      isLiked = existingRecord[0]["add"];
-      print(existingRecord[0]["add"]);
-
-      print("ここが一つ目");
-      final response = await supabase
+    try {
+      // `user_id`と`image_id`の組み合わせで既存のレコードを検索する
+      final existingRecord = await supabase
           .from('likes')
-          .update({ 'add': isLiked })
+          .select<List<Map<String, dynamic>>>()
           .eq('user_id', userId)
           .eq('image_id', widget.image_id);
 
-      if (response != null) {
-        // エラーハンドリング
-        print('Error updating data: ${response}');
-      } else {
-        // 成功時の処理
-        print('Data updated successfully!');
-      }
-    } else {
-      print("ここが二つ目");
-      // レコードが存在しない場合は挿入
-      final response = await supabase
-          .from('likes')
-          .insert({
-            'user_id': userId,
-            "image_own_user_id": widget.image_own_user_id,
-            "add": false,
-            "problem_num": widget.num,
-            "image_id": widget.image_id,
-            // 他のカラムの挿入もここで行う
-          });
+      // レコードが存在する場合はアップデート、存在しない場合は挿入する
+      if (existingRecord.isNotEmpty) {
+        // レコードが存在する場合はアップデート
+        print("ここが問題手の");
+        isLiked = existingRecord[0]["add"];
+        print(existingRecord[0]["add"]);
 
-      
-      print("here");
-      print(response);
+        print("ここが一つ目");
+        final response = await supabase
+            .from('likes')
+            .update({ 'add': isLiked })
+            .eq('user_id', userId)
+            .eq('image_id', widget.image_id);
 
-      if (response == null) {
-        // エラーハンドリング
-        print('Error inserting data: ${response}');
+        if (response != null) {
+          // エラーハンドリング
+          print('Error updating data: ${response}');
+        } else {
+          // 成功時の処理
+          print('Data updated successfully!');
+        }
       } else {
-        // 成功時の処理
-        print('Data inserted successfully!');
+        print("ここが二つ目");
+        // レコードが存在しない場合は挿入
+        final response = await supabase
+            .from('likes')
+            .insert({
+              'user_id': userId,
+              "image_own_user_id": widget.image_own_user_id,
+              "add": false,
+              "problem_num": widget.num,
+              "image_id": widget.image_id,
+              // 他のカラムの挿入もここで行う
+            });
+
+        
+        print("here");
+        print(response);
+
+        if (response == null) {
+          // エラーハンドリング
+          print('Error inserting data: ${response}');
+        } else {
+          // 成功時の処理
+          print('Data inserted successfully!');
+        }
       }
+    } catch (error) {
+      // 例外が発生した場合のエラーハンドリング
+      print('Error: $error');
     }
-  } catch (error) {
-    // 例外が発生した場合のエラーハンドリング
-    print('Error: $error');
   }
-}
+
+  void _showCommentSheet(BuildContext context, int imageId) {
+    showModalBottomSheet(
+      context: context,
+      //これがないと高さが変わらない
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Container(
+          height: SizeConfig.blockSizeVertical! * 60,
+          child: CommentListDisplay(image_id: imageId),
+        );
+      },
+    );
+  }
 
 
   @override
@@ -241,6 +263,60 @@ class _DisplayPageState extends State<DisplayPage>{
      return Scaffold(
       appBar: AppBar(
         title: const Text('画像一覧'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.chat, color: Colors.green,),
+            tooltip: "コメント",
+            onPressed: (){
+              _showCommentSheet(context, widget.image_id!);
+            },
+          ),
+
+          SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),
+
+          //ここで問題の評価を見る
+          IconButton(
+            icon: const Icon(Icons.bar_chart, color: Colors.blue,),
+            tooltip: "評価",
+            onPressed: (){
+              _showCommentSheet(context, widget.image_id!);
+            },
+          ),
+
+          SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),
+
+          //ここで問題の評価を行う
+          IconButton(
+            icon: const Icon(Icons.edit, color: Colors.white,),
+            tooltip: "評価する",
+            onPressed: (){
+              _showCommentSheet(context, widget.image_id!);
+            },
+          ),
+
+          SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),
+
+          IconButton(
+            icon: Icon(
+              isLiked ? Icons.favorite : Icons.favorite_border_outlined,
+              color: isLiked ? Colors.red : Colors.white,
+            ),
+            
+            tooltip: "いいね",
+            onPressed: () async{
+              
+              await _insertTestSupabase();
+
+              setState(() {            
+                isLiked = !isLiked;
+              });
+            },
+          ),
+
+          SizedBox(width: SizeConfig.blockSizeHorizontal! * 2,),
+
+
+        ],
       ),
 
       body: SingleChildScrollView(
@@ -342,22 +418,7 @@ class _DisplayPageState extends State<DisplayPage>{
 
       ),
 
-      //ここでいいね評価を行う
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async { 
-          await _insertTestSupabase();
-
-          setState(() {            
-            isLiked = !isLiked;
-          });
-          
-        },
-        //もしisLikedがtrueならばアイコンは黄色、falseならば白
-        child: Icon(
-          isLiked ? Icons.favorite : Icons.favorite_border_outlined,
-          color: isLiked ? Colors.red : Colors.white,
-        ),
-      )
+      
 
 
 
@@ -366,3 +427,214 @@ class _DisplayPageState extends State<DisplayPage>{
 
   }
 }
+
+
+
+
+class CommentListDisplay extends StatefulWidget{
+  
+    //final String? comment;
+    //final String? created_at;
+    final int? image_id;
+  
+    const CommentListDisplay({
+      Key? key,
+     //required this.comment,
+      //required this.created_at,
+      required this.image_id,
+    }) : super(key: key);
+  
+    @override
+    _CommentListDisplayState createState() => _CommentListDisplayState();
+}
+
+class _CommentListDisplayState extends State<CommentListDisplay>{
+
+  late List<Map<String, dynamic>> _commentList = [];
+
+  late final TextEditingController _textController = TextEditingController();
+
+  /*
+  Future<void> fetchData() async{
+
+    _commentList = await supabase
+          .from('comments')
+          .select<List<Map<String, dynamic>>>()
+          .eq('image_id', widget.image_id)
+          .order("created_at");
+
+    print(_commentList);
+  }
+   */
+
+  Future<List<Map<String, dynamic>>> fetchData() async {
+  // 非同期処理を行う（例: データの取得）
+  List<Map<String, dynamic>> data = await supabase
+          .from('comments')
+          .select<List<Map<String, dynamic>>>()
+          .eq('image_id', widget.image_id)
+          .order("created_at");
+  return data; // 結果をFutureで包んで返す
+}
+
+
+  /// メッセージを送信する
+  void _submitMessage() async {
+    final comment = _textController.text;
+    if (comment.isEmpty) {
+      context.showErrorSnackBar(message: "コメントが入力されていません。");
+      return;
+    }
+    _textController.clear();
+    try {
+      await supabase.from('comments').insert({
+        "user_id": myUserId,
+        'comments': comment,
+        "image_id": widget.image_id,
+      });
+
+
+    } on PostgrestException catch (error) {
+      // エラーが発生した場合はエラーメッセージを表示
+      context.showErrorSnackBar(message: error.message);
+    } catch (_) {
+      // 予期せぬエラーが起きた際は予期せぬエラー用のメッセージを表示
+      context.showErrorSnackBar(message: unexpectedErrorMessage);
+    }
+
+    fetchData().then((data) {
+      setState(() {
+        _commentList = data;
+      });
+    });
+
+    //context.showSuccessSnackBar(message: "コメントを送信しました。コメントを見たい場合にはリロードしてください");
+    
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    //これで非同期的
+    fetchData().then((data) {
+      setState(() {
+        _commentList = data;
+      });
+    });
+
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    _textController.text = textKeeper;
+
+    //コメント一覧を表示する
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.green),
+        borderRadius: BorderRadius.circular(10),
+      ),
+
+      //height: SizeConfig.blockSizeVertical! * 80,
+      width: SizeConfig.blockSizeHorizontal! * 95,
+
+      child: Column(
+        children: [
+
+          Container(
+            child: Row(
+              children: [
+                const Expanded(
+                  child: Center(
+                    child: Opacity(
+                      opacity: 0.5,
+                      child: Text(
+                                "コメント一覧",
+                                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,fontStyle: FontStyle.italic)
+                              ),
+                    ),
+                  ),
+                ),
+
+                IconButton(
+
+                  onPressed: (){
+
+                    fetchData().then((data) {
+                      setState(() {
+                        _commentList = data;
+                      });
+                    });
+
+                  },
+                  //reload
+                  icon: const Icon(Icons.refresh),
+                )
+              ],
+            ),
+          ),
+          
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: _commentList.length,
+              itemBuilder: (BuildContext context, int index){
+                return ChatBubble(commentData: _commentList[index]);
+              },
+
+            )
+          ),
+
+          Material(
+            color: Colors.black12,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3, // 複数行入力可能にする
+                      controller: _textController,
+                      decoration: const InputDecoration(
+                        hintText: 'メッセージを入力',
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        contentPadding: EdgeInsets.all(8),
+                      ),
+
+                      onChanged: (text) {
+                        textKeeper = text;
+                      },
+
+                    ),
+                  ),
+
+                  TextButton(
+                    //onPressed: () => _submitMessage(),
+                    onPressed: () {
+                      _submitMessage();
+                    },
+                    child: const Text('送信'),
+                  ),
+                ],
+              ),
+            )
+          ),
+
+        ],
+      ),
+
+
+    );
+  }
+
+}
+
+
